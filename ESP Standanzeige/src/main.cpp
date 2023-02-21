797ede562d2a75d67d27384979ccbb0d1d38047e
tobsolutions <tobi@tobsolutions.de>
2023-02-21 12:23:25 +0100

    #define ERST_STAND

--- ESP Standanzeige/src/main.cpp
+++ ESP Standanzeige/src/main.cpp
@@ -7,6 +7,7 @@
 #include "Adafruit_MCP23X17.h"
 
 #define ANZ_STAENDE 12
+#define ERST_STAND 1
 #define SIMULATION_MODE true
 
 struct settings {
@@ -56,7 +57,7 @@ void handleFreeLanes() {
     String html;
     html = "<!doctype html><html lang='en'><head><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><title>ESP Standanzeige</title> <style>*,::after,::before{box-sizing:border-box;}body{margin:0;font-family:'Segoe UI',Roboto,'Helvetica Neue',Arial,'Noto Sans','Liberation Sans';font-size:1rem;font-weight:400;line-height:1.5;color:#212529;background-color:#f5f5f5;}.form-control{display:block;width:100%;height:calc(1.5em + .75rem + 2px);border:1px solid #ced4da;}button{cursor: pointer;border:1px solid transparent;color:#fff;background-color:#007bff;border-color:#007bff;padding:.5rem 1rem;font-size:1.25rem;line-height:1.5;border-radius:.3rem;width:100%}.form-signin{width:100%;max-width:400px;padding:15px;margin:auto;}h1{text-align: center}</style> </head> <body><main class='form-signin'><h2>ESP Standanzeige</h2>";
     html += "<p>";
-    for(int i = 1;i <= ANZ_STAENDE;i++){
+    for(int i = ERST_STAND;i <= ANZ_STAENDE+ERST_STAND;i++){
       html += "Stand ";
       html += i;
       if(freeLanes[i] == 1){
@@ -144,7 +145,7 @@ void loop()
 		  ws.connect("192.168.10.200","/",49472);
     } else {
       Serial.println("## SIMULATION MODE ## No connection to websocket. Random free Lanes.");
-      for(int i = 1;i <= ANZ_STAENDE;i++){
+      for(int i = ERST_STAND;i <= ANZ_STAENDE+ERST_STAND;i++){
         int rand = random(100);
         if(rand > 50) {
           freeLanes[i] = 1;
@@ -176,7 +177,7 @@ void loop()
       JsonArray data = msg_rx_json["Data"];
       //Serial.print("data[0]: ");
       //Serial.println(data[0].as<String>());
-      for(int i = 1;i <= ANZ_STAENDE;i++){
+      for(int i = ERST_STAND;i <= ANZ_STAENDE+ERST_STAND;i++){
         freeLanes[i] = 0;
       }
       for(JsonVariant v : data){
@@ -185,14 +186,14 @@ void loop()
       }
 		}
 	}
-  for(int i = 1;i <= ANZ_STAENDE;i++){
+  for(int i = ERST_STAND;i <= ANZ_STAENDE+ERST_STAND;i++){
     if(freeLanes[i] == 1){
       Serial.print("Stand ");
       Serial.print(i);
       Serial.println(" ist frei");
-      mcp0.digitalWrite(i, LOW);
+      mcp0.digitalWrite(i-ERST_STAND, LOW);
     } else {
-      mcp0.digitalWrite(i, HIGH);
+      mcp0.digitalWrite(i-ERST_STAND, HIGH);
     }
   }
 	delay(2000);
